a8ad3d2aa9991257d1474adb82c5dda01bcd19dc
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-01-27 08:11:46 +0000

    aggiunta classe per creare i personaggi

---
+++ Personaggio.java
@@ -0,0 +1,17 @@
+public class Personaggio {
+    String nome;
+    String coloreCapelli;
+    boolean occhiali;
+    boolean cappello;
+    boolean barba;
+    boolean baffi;
+
+    public Personaggio(String nome, String coloreCapelli, boolean occhiali, boolean cappello, boolean barba, boolean baffi) {
+        this.nome = nome;
+        this.coloreCapelli = coloreCapelli;
+        this.occhiali = occhiali;
+        this.cappello = cappello;
+        this.barba = barba;
+        this.baffi = baffi;
+    }
+}
