bf6dca3065dbad657bd6be759e649ae7e30d7083
SimoneArzuffi <101709449+SimoneArzuffi@users.noreply.github.com>
2024-02-10 07:19:56 +0000

    implementato multiclient

--- Server.java
+++ Server.java
@@ -7,94 +7,22 @@ public class Server {
     private static String parteiniziale2 = "Il personaggio ha i capelli";
     private static String parteiniziale3 = "Il personaggio si chiama";
     private static Personaggio[] personaggi = new Personaggio[12];
-    private static String[] paroleFrase;
-       public static void main(String[] args) {
+    
+    public static void main(String[] args) {
         creaPersonaggi();
-        int numeroCasuale = numeroCasuale(personaggi.length);
         try (ServerSocket serverSocket = new ServerSocket(1234)) {
             System.out.println("Server is listening on port 1234");
-            
+
             while (true) {
-                try (Socket socket = serverSocket.accept();
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    System.out.println("Numero scelto: " + numeroCasuale);
-                    String inputLine;
-                    while ((inputLine = in.readLine()) != null) {
-                        System.out.println("Server received: " + inputLine);
-                        // Se l'utente scrive QUIT, il server si disconnette
-                        if (inputLine.equals("QUIT")) {
-                            break;
-                        }else{
-                            //controllo input client con domande ammesse
-                            if(inputLine.startsWith(parteiniziale1)){
-                                //out.println("ricevuto genere");
-                                paroleFrase = inputLine.split(" ");
-                                if(paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getGenere())){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else if(inputLine.startsWith(parteiniziale2)){
-                                //out.println("ricevuto capelli");
-                                paroleFrase = inputLine.split(" ");
-                                if(paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getColoreCapelli())){
-                                    out.println("SI");
-                                }else if (paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getColoreCapelli())){
-                                    out.println("SI");
-                                }else if (paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getColoreCapelli())){
-                                    out.println("SI");
-                                }else if (paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getColoreCapelli())){
-                                    out.println("SI");
-                                }else if (paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getColoreCapelli())){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else if(inputLine.equals("Il personaggio ha gli occhiali ?")){
-                                //out.println("ricevuto occhiali");
-                                if(personaggi[numeroCasuale].getOcchiali()){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else if(inputLine.equals("Il personaggio ha il cappello ?")){
-                                //out.println("ricevuto capello");
-                                if(personaggi[numeroCasuale].getCappello()){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else if(inputLine.startsWith(parteiniziale3)){
-                                //out.println("ricevuto nome");
-                                paroleFrase = inputLine.split(" ");
-                                if(paroleFrase[paroleFrase.length - 2].equals(personaggi[numeroCasuale].getNome())){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else if(inputLine.equals("Il personaggio ha la barba ?")){
-                                //out.println("ricevuto barba");
-                                if(personaggi[numeroCasuale].getBarba()){
-                                    out.println("SI");
-                                }else{
-                                    out.println("NO");
-                                }
-                            }else{
-                                out.println("Domanda non ammessa");
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    System.out.println("Eccezione rilevata durante il tentativo di ascolto sulla porta 1234 o l'ascolto di una connessione");
-                    System.out.println(e.getMessage());
-                }
+                Socket socket = serverSocket.accept();
+                new Thread(new ClientHandler(socket)).start();
             }
         } catch (IOException e) {
             System.out.println("Impossibile connettersi sulla porta 1234");
             System.out.println(e.getMessage());
         }
     }
+
 
     private static void creaPersonaggi(){
         personaggi[0] = new Personaggio("Tommaso", "maschio", "neri", false,true, false);
@@ -113,4 +41,82 @@ public class Server {
     private static int numeroCasuale(int max){
         return (int)(Math.random() * max);
     }
+
+    private static class ClientHandler implements Runnable {
+        private Socket socket;
+        private static String[] paroleFrase;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+            ) {
+                int numeroCasuale = numeroCasuale(personaggi.length);
+                System.out.println("Connected to client. Numero scelto: " + numeroCasuale);
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Server received: " + inputLine);
+                    // Se l'utente scrive QUIT, il server si disconnette
+                    if (inputLine.equals("QUIT")) {
+                        break;
+                    }
+                    String[] paroleFrase;
+                    //controllo input client con domande ammesse
+                    if(inputLine.startsWith(parteiniziale1)){
+                        paroleFrase = inputLine.split(" ");
+                        if(paroleFrase[paroleFrase.length - 1].equals(personaggi[numeroCasuale].getGenere())){
+                            out.println("SI");
+                        }else{
+                            out.println("NO");
+                        }
+                    }else if(inputLine.startsWith(parteiniziale2)){
+                        paroleFrase = inputLine.split(" ");
+                        if(paroleFrase[paroleFrase.length - 1].equals(personaggi[numeroCasuale].getColoreCapelli())){
+                            out.println("SI");
+                        }else{
+                            out.println("NO");
+                        }
+                    }else if(inputLine.startsWith(parteiniziale3)){
+                        paroleFrase = inputLine.split(" ");
+                        if(paroleFrase[paroleFrase.length - 1].equalsIgnoreCase(personaggi[numeroCasuale].getNome())){
+                            out.println("SI");
+                        }else{
+                            out.println("NO");
+                        }
+                    }else{
+                        switch (inputLine) {
+                            case "Il personaggio ha gli occhiali ?":
+                                out.println(personaggi[numeroCasuale].getOcchiali() ? "SI" : "NO");
+                                break;
+                            case "Il personaggio ha il cappello ?":
+                                out.println(personaggi[numeroCasuale].getCappello() ? "SI" : "NO");
+                                break;
+                            case "Il personaggio ha la barba ?":
+                                out.println(personaggi[numeroCasuale].getBarba() ? "SI" : "NO");
+                                break;
+                            default:
+                                out.println("Domanda non ammessa");
+                                break;
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port or listening for a connection");
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println("Error closing the socket");
+                }
+            }
+        }
+
+    }
+
 }
